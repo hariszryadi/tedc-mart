2a9843fddff1d5522c727691f9d4e63d58555393
hariszryadi <harisz.ryadi17@gmail.com>
2020-07-29 15:44:43 +0700

    initial commit

---
+++ src/views/About.vue
@@ -0,0 +1,85 @@
+<template>
+  <div>
+    <v-container>
+      <h4>Created By</h4>
+      <v-list-item>
+          <v-list-item-avatar height="100px" width="100px">
+              <v-img src="img/harisz.png" ></v-img>
+          </v-list-item-avatar>
+          <v-list-item-content>
+              <v-list-item-title>Nama</v-list-item-title>
+              <v-list-item-subtitle>Muhammad Harisz Ryadi</v-list-item-subtitle>
+              <v-divider></v-divider>
+              <v-list-item-title>Tempat, Tanggal Lahir</v-list-item-title>
+              <v-list-item-subtitle>17 Oktober 1995</v-list-item-subtitle>
+              <v-divider></v-divider>
+              <v-list-item-title>Alamat</v-list-item-title>
+              <v-list-item-subtitle>Jl. Tanimulya Raya, Bandung Barat</v-list-item-subtitle>
+              <v-divider></v-divider>
+              <v-list-item-title>Hobi</v-list-item-title>
+              <v-list-item-subtitle>Ngoding</v-list-item-subtitle>
+          </v-list-item-content>
+      </v-list-item>
+      <v-list-item>
+          <v-list-item-content>
+              <v-list-item-title>Nama</v-list-item-title>
+              <v-list-item-subtitle>Yussi Sri Rezeki</v-list-item-subtitle>
+              <v-divider></v-divider>
+              <v-list-item-title>Tempat, Tanggal Lahir</v-list-item-title>
+              <v-list-item-subtitle>14 Oktober 1996</v-list-item-subtitle>
+              <v-divider></v-divider>
+              <v-list-item-title>Alamat</v-list-item-title>
+              <v-list-item-subtitle>Jl. Sangkuriang, Cimahi</v-list-item-subtitle>
+              <v-divider></v-divider>
+              <v-list-item-title>Hobi</v-list-item-title>
+              <v-list-item-subtitle>Nonton Drakor</v-list-item-subtitle>
+          </v-list-item-content>
+          <v-list-item-avatar height="100px" width="100px">
+              <v-img src="img/uci.png" ></v-img>
+          </v-list-item-avatar>
+      </v-list-item>
+      <!-- <v-card flat>
+        <v-img src="img/harisz.png" height="150px" width="150px"></v-img>
+        <v-container>
+            <v-simple-table>
+                <tbody>
+                    <tr>
+                        <th>Nama</th>
+                        <td>Muhammad Harisz Ryadi</td>
+                    </tr>
+                    <tr>
+                        <th>Tempat, Tanggal Lahir</th>
+                        <td>17 Oktober 1995</td>
+                    </tr>
+                    <tr>
+                        <th>Alamat</th>
+                        <td>Jl. Tanimulya Raya 37, Ngamprah, Bandung Barat</td>
+                    </tr>
+                </tbody>
+            </v-simple-table>
+        </v-container>
+      </v-card> -->
+      <!-- <v-card flat>
+        <v-img src="img/uci.png" height="150px" width="150px"></v-img>
+        <v-container>
+            <v-simple-table>
+                <tbody>
+                    <tr>
+                        <th>Nama</th>
+                        <td>Yussi Sri Rezeki</td>
+                    </tr>
+                    <tr>
+                        <th>Tempat, Tanggal Lahir</th>
+                        <td>14 Oktober 1996</td>
+                    </tr>
+                    <tr>
+                        <th>Alamat</th>
+                        <td>Kav. Sangkuriang, Cimahi Tengah, Cimahi</td>
+                    </tr>
+                </tbody>
+            </v-simple-table>
+        </v-container>
+      </v-card> -->
+    </v-container>
+  </div>
+</template>
